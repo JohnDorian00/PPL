bd9fbe92309bf7b100699fdbfdc5fe81fd4e194c
JohnDorian00 <remailfunny@gmail.com>
2020-07-16 17:09:01 +0300

    table layout

--- src/IERT/vue/MainWindow.vue
+++ src/IERT/vue/MainWindow.vue
@@ -12,9 +12,7 @@
              @close="closeWindows">
     <!--             :title="title"-->
     <div ref="header" style="position: relative;">
-
       <div style="display: inline;">{{title}}</div>
-
       <div style="display: inline; position: absolute; top:0; right: 0;
                   margin-top: 6px; margin-right: 5px; z-index: 99999999999999; cursor: pointer;" @click="closeWindows"
       >
@@ -28,48 +26,52 @@
           <img class="close-button"  src="@/style/images/closing.png" @click="closeWindows">
         </div>
       </div>
-
-
     </div>
-    <div>
-      <div>
-<!--        TODO разобраться с высотой таблицы (прикрепить к div с нижними кнопками)-->
-        <JqxGrid ref="myGrid" :width="'100%'" :source="dataAdapter"
-                 :columns="columns" :pageable="true" :autoheight="true"
+
+    <div ref="content" style=" top: 0px; width: 100%; background-color: rgb(0,0,255); ">
+
+      <div style="background-color: rgb(255,0,0); width: 100%; position: absolute; top: 0px; margin-top: 31px; margin-right: 60px" :style="{bottom: button_height+'px'}">
+
+<!--        <div style="height: 31px; width: 100%; position:relative; top: 0px"></div>-->
+        <JqxGrid style="position:relative; height: 100%" ref="myGrid" :width="'100%'" :source="dataAdapter" :columnsmenu="false"
+                 :columns="columns" :pageable="false" :autoheight="false"
                  :sortable="true" :altrows="true" :enabletooltip="true"
-                 :editable="true" :selectionmode="'multiplecellsadvanced'" >
+                 :editable="false" :selectionmode="'singlerow'" >
         </JqxGrid>
+
+
       </div>
 
-      <div class="btn-group">
-        <JqxButton  ref="myTextImageButton1" @click="this.$root.$children[0].createWindowNewVariant" :width="120" :height="40"
-                    :textImageRelation="'imageBeforeText'" :textPosition="'left'"
-                    :theme="theme" :style="{'display': 'inline-block'}"
-        >Создать новый вариант
-        </JqxButton>
-        <JqxButton  ref="myTextImageButton2" :width="120" :height="40"
-                    :textImageRelation="'imageBeforeText'" :textPosition="'left'"
-                    :theme="theme" :style="{'display': 'inline-block'}"
-        >
-        </JqxButton>
-        <JqxButton  ref="myTextImageButton3" :width="120" :height="40"
-                    :textImageRelation="'imageBeforeText'" :textPosition="'left'"
-                    :theme="theme" :style="{'display': 'inline-block'}"
-        >
-        </JqxButton>
-        <JqxButton  @click="updateGridFromURL" ref="refreshTable" :width="120" :height="40"
-                    :textImageRelation="'imageBeforeText'" :textPosition="'left'"
-                    :theme="theme" :style="{'display': 'inline-block'}"
-        >Обновить
-        </JqxButton>
-        <JqxButton  ref="closeButton" @click="closeWindows" :width="120" :height="40"
-                    :textImageRelation="'imageBeforeText'" :textPosition="'left'"
-                    :theme="theme" :style="{ 'display': 'inline-block'}"
-        >Закрыть
-        </JqxButton>
+      <div class="btn-group" >
+<!--        :style="{ bottom: button_height+'px' }"-->
+          <JqxButton  ref="myTextImageButton1" @click="this.$root.$children[0].createWindowNewVariant" :width="120" :height="button_height"
+                      :textImageRelation="'imageBeforeText'" :textPosition="'left'"
+                      :theme="theme" :style="{'display': 'inline-block'}"
+          >Создать новый вариант
+          </JqxButton>
+          <JqxButton  ref="myTextImageButton2" :width="120" :height="button_height+'px'"
+                      :textImageRelation="'imageBeforeText'" :textPosition="'left'"
+                      :theme="theme" :style="{'display': 'inline-block'}"
+          >
+          </JqxButton>
+          <JqxButton  ref="myTextImageButton3" :width="120" :height="button_height+'px'"
+                      :textImageRelation="'imageBeforeText'" :textPosition="'left'"
+                      :theme="theme" :style="{'display': 'inline-block'}"
+          >
+          </JqxButton>
+          <JqxButton  @click="updateGridFromURL" ref="refreshTable" :width="120" :height="button_height+'px'"
+                      :textImageRelation="'imageBeforeText'" :textPosition="'left'"
+                      :theme="theme" :style="{'display': 'inline-block'}"
+          >Обновить
+          </JqxButton>
+          <JqxButton  ref="closeButton" @click="closeWindows" :width="120" :height="button_height+'px'"
+                      :textImageRelation="'imageBeforeText'" :textPosition="'left'"
+                      :theme="theme" :style="{ 'display': 'inline-block'}"
+          >Закрыть
+          </JqxButton>
       </div>
 
-    </div>
+  </div>
   </JqxWindow>
 </template>
 
@@ -91,6 +93,7 @@
     data() {
       return {
         theme: appConfig.windowsTheme,
+        button_height: 30,
         dataAdapter: new jqx.dataAdapter(this.source),
         columns: [
           { text: 'id', datafield: 'var_id', width: '44'},
@@ -124,6 +127,13 @@
           t.$refs.myGrid.updatebounddata();
           xmlQuery.destroy();
           console.log("Success update data");
+
+          // adding data for test
+          for (let i=0; i<15; i++) {
+            t.$refs.myGrid.addrow(1,[
+              {var_id: i, var_year: "asd", var_gs_var_id: "asd", var_name: "asd", gs_name:"asd", var_desc: "asd"},
+            ]);
+          }
         }
       }
     },
@@ -147,11 +157,12 @@
 
 <style scoped>
   .btn-group {
-    background-color: #545454;
+    /*background-color: #545454;*/
+    background-color: green;
     position: absolute;
     left: 0;
     bottom: 0;
-    width: 100%;
+    width: 80%;
   }
 
   .collapse-button {
